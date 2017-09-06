a8fa8f5d8026e5f1103c4e8f0953be828052a180
Matthew Brown <lokislayer@gmail.com>
2017-09-05 21:17:59 -0500

    Actual Single Dice Representation

---
+++ RollDiceCore/RollDiceCore/Src/Die.cs
@@ -0,0 +1,35 @@
+﻿namespace RollDiceCore.Src
+{
+    /// <summary>
+    /// This visualizes what a single die[example: D6] used in any games.
+    /// </summary>
+    public class Die
+    {
+        /// <summary>
+        /// Enum version of the Die in question
+        /// </summary>
+        protected DiceTypes _Die { get; set; } = DiceTypes.None;
+
+        /// <summary>
+        /// The number of sides
+        /// </summary>
+        protected int Sides { get; set; }
+
+        /// <summary>
+        /// Amount for when the dice is actaully rolled
+        /// </summary>
+        protected int Amonut { get; set; }
+
+        public Die(int sides)
+        {
+            Sides = sides;
+            _Die = Extensions.ConvertToTypes(Sides);
+        }
+
+        public Die(DiceTypes die)
+        {
+            Sides = die.ConvertToInt();
+            _Die = die;
+        }
+    }
+}
