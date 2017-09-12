eab1d22620d2c2a4a299b8f4893ed70c173c2fb3
Matthew Brown <lokislayer@gmail.com>
2017-09-12 07:39:29 -0500

    Simple Wrapper for calling Random
    Simple wrapper for calls to using Random. It is a singleton which should not have colliisions.

---
+++ RollDiceCore/RollDiceCore/Roller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RollDiceCore
+{
+    public class Roller
+    {
+        private static Roller _rollerInstance = null;
+        private Random _random;
+
+        private Roller()
+        {
+            _random = new Random();
+        }
+
+        public static Roller GetInstance()
+        {
+            if (_rollerInstance == null)
+            {
+                _rollerInstance = new Roller();                
+            }
+
+            return _rollerInstance;
+        }
+
+        public Random RAND => _random;
+    }
+}
