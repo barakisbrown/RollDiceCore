058c35f1eea3087e3b752c9e6b14f8dc688e4cbb
Matthew Brown <lokislayer@gmail.com>
2017-09-12 08:14:29 -0500

    Simple DiceRoller
    Used for basic dice rolling such a 1D4 or 4D4.

---
+++ RollDiceCore/RollDiceCore/Regular.cs
@@ -0,0 +1,20 @@
+﻿namespace RollDiceCore
+{
+    public class Regular
+    {
+        private readonly Roller _diceRoller = Roller.GetInstance();
+        private readonly DiceTypes _die;
+        private readonly int _sides;
+
+        public Regular(DiceTypes type)
+        {
+            _die = type;
+            _sides = type.ConvertToInt();
+        }
+
+        public int Roll()
+        {
+            return _diceRoller.RAND.Next(1, _sides);
+        }
+    }
+}
