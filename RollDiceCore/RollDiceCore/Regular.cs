0f9e88b778da409bfe3c292a7e312a99a31a4380
Matthew Brown <lokislayer@gmail.com>
2017-09-13 10:21:00 -0500

    Added Documentation

--- RollDiceCore/RollDiceCore/Regular.cs
+++ RollDiceCore/RollDiceCore/Regular.cs
@@ -1,5 +1,10 @@
 ﻿namespace RollDiceCore
 {
+    /// <summary>
+    /// Dice roller used for most cases where a dice needs to be rolled.
+    /// Yes, u could use rand function yourself but this wrap around it
+    /// and automatically give u a result between 1 and N.
+    /// </summary>
     public class Regular
     {
         private readonly Roller _diceRoller = Roller.GetInstance();
@@ -12,6 +17,10 @@
             _sides = type.ConvertToInt();
         }
 
+        /// <summary>
+        /// Simple rolls a die and returns it
+        /// </summary>
+        /// <returns>1 to N where N is the dice rolled</returns>
         public int Roll()
         {
             return _diceRoller.RAND.Next(1, _sides);
