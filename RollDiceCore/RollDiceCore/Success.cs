c5c57b7957e11528e04f68e1986ca644e6e24c14
Matthew Brown <lokislayer@gmail.com>
2017-09-30 13:25:14 -0500

    Added new overloaded function
    Added new function that will return number of success if any that where rolled. It needs two arrays which is how the results are returned back to the caller function.

--- RollDiceCore/RollDiceCore/Success.cs
+++ RollDiceCore/RollDiceCore/Success.cs
@@ -1,5 +1,6 @@
 ﻿namespace RollDiceCore
 {
+    using System;
     /// <summary>
     /// An success is when dice a rolled against a target number to determine if the action
     /// was good or not. Games such as Storyteller, Shadowrun, others.
@@ -42,6 +43,40 @@
             var diceRolled = _dice.Roll();
             needResult = diceRolled;
             return (diceRolled >= _targetNumber);
+        }
+
+        /// <summary>
+        /// This overloaded version of Roll lets the GM Roll multiple dice
+        /// and get their results back for both if they succeeded and what
+        /// was rolled
+        /// </summary>
+        /// <param name="results">Array of boolean values for success</param>
+        /// <param name="rolled">Array of integers valuea of actual results
+        /// <returns>Number of success rolled</returns>
+        public int Roll(int numberTimes, out bool []results, out int []rolled)
+        {
+            
+            // Initalize arrays that need to be returned
+            if (numberTimes < 1)
+                throw new System.ArgumentOutOfRangeException("numberTimes can not be less than 1");
+            results = new bool[numberTimes];
+            rolled = new int[numberTimes];
+
+            int successRolled = 0;
+            // Start Iterating and return how many successes where rolled if any
+
+            for (int i = 0; i < numberTimes; i++)
+            {
+                bool success;
+                int resultRolled;
+                success = Roll(out resultRolled);
+                if (success)
+                    successRolled++;
+                results[i] = success;
+                rolled[i] = resultRolled;
+            }
+
+            return successRolled;
         }
     }
 }
