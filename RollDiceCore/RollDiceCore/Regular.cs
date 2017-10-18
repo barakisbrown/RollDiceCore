1316027b4e139e210712e4fc53b72cc2a926c24c
Matthew Brown <lokislayer@gmail.com>
2017-10-18 12:22:01 -0500

    Updated Roll(int) and Added new method
    Roll(int, out int[])

--- RollDiceCore/RollDiceCore/Regular.cs
+++ RollDiceCore/RollDiceCore/Regular.cs
@@ -1,4 +1,6 @@
-﻿namespace RollDiceCore
+﻿using System;
+
+namespace RollDiceCore
 {
     /// <summary>
     /// Dice roller used for most cases where a dice needs to be rolled.
@@ -35,12 +37,38 @@
         public int Roll(int numberOfTimes)
         {
             if (numberOfTimes < 1)
-                throw new System.ArgumentException("Number of dice has to be at least 1");
+                throw new System.ArgumentException("Number of dice has to be greater than 1");
 
             int resultReturned = 0;
             for (int i = 0; i < numberOfTimes; i++)
             {
                 resultReturned += Roll();
+            }
+
+            return resultReturned;
+        }
+
+        /// <summary>
+        /// Rolls the same dice a predefined number of times based on the argument supplied
+        /// IE: numberOfTimes = 5, then it will roll the dice 5 times and then return the result
+        /// This method also returns an array of values should the user need the results for display,etc.
+        /// </summary>
+        /// <param name="numberOfTimes">The number of dice rolled. Has to be greater than 1</param>
+        /// <param name="results">Integer Array where results are returned back to the caller function. Please note, 
+        ///                       the array being returned will have been recreated.</param>
+        /// <returns>Amount Rolled</returns>
+        public int Roll(int numberOfTimes, out int[]results)
+        {
+            if (numberOfTimes < 1)
+                throw new ArgumentException("Number of dice has to be greater than 1");
+
+            results = new int[numberOfTimes];
+            int resultReturned = 0;
+            for (int I = 0; I < numberOfTimes; I++)
+            {
+                var tempRolled = Roll();
+                resultReturned += tempRolled;
+                results[I] = tempRolled;
             }
 
             return resultReturned;
